c60a47aae3fa2e69df3c168c558d00285202cf15
zuoshuhua@sina.com <git>
2019-03-25 19:40:06 +0800

    hzrservice

--- roles/checkenv/files/start_registry.sh
+++ roles/checkenv/files/start_registry.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it -d --name registry -p 5000:5000 -v /opt/docker/config.yml:/opt/docker/config.yml -v /registry-data01:/var/lib/registry registry:2.3.1   /opt/docker/config.yml
+docker run -it -d --name registry -p 5000:5000 -v /opt/docker/config.yml:/opt/docker/config.yml -v /registry-data01:/var/lib/registry registry:2.5.2   /opt/docker/config.yml
